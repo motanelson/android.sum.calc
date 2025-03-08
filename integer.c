a5a92247c25e9d1acabdad7c3bbd008bbb1c16ed
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2025-03-08 13:35:16 +0000

    Add files via upload

--- integer.c
+++ integer.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+int num=1000;
+int mults(int values){
+    return values*num;
+}
+int divsn(int values){
+    return values/num;
+}
+int matsn(int min, int max,int values){
+    return (max-min)/values;
+
+}
+
 int main(){
     
     int n=0;
@@ -10,14 +22,14 @@ int main(){
     int divs=9;
     int t=1;
     char nm[1024]="\0"; 
-    n=100*1000;
-    printf("\033[43;30m%d=%d\n",n,n/1000);
-    nnn=200*1000;
-    i=(nnn-n)/divs;
+    n=mults(100);
+    printf("\033[43;30m%d=%d\n",n,divsn(n));
+    nnn=mults(200);
+    i=matsn(n,nnn,divs);
     while(t){
         n=n+i;
         if(n<nnn){
-            printf("\033[43;30m%d=%d\n",n,n/1000);
+            printf("\033[43;30m%d=%d\n",n,divsn(n));
         }else{
             t=0;
         }
