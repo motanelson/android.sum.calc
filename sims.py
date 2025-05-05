93931b9c7f439721b56d1e0fd88f9925263814f8
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2025-05-05 11:47:08 +0100

    Add files via upload

---
+++ sims.py
@@ -0,0 +1,12 @@
+
+def starts(a):
+    print(a)
+def ends(a):
+    print(a)
+def bodys(a):
+    print(a)
+
+print ("\033c\033[43;30m\nenter simulator\n")
+starts("starts....")
+bodys("works.......")
+ends("ends.........")
