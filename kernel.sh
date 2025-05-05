752f855c4af0054c3f77038fcf28c356dd4a3586
nelson jr mota <118323821+motanelson@users.noreply.github.com>
2025-05-05 11:46:55 +0100

    Delete kernel.sh

--- kernel.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/sh 
-printf "\033c\033[43;30m\ndownloa gcc"
-wget "https://git.kernel.org/torvalds/t/linux-6.15-rc1.tar.gz" 
-gzip -d 'linux-6.15-rc1.tar.gz'
-tar --extract -f 'linux-6.15-rc1.tar'
-cd linux-6.15-rc1
- 
